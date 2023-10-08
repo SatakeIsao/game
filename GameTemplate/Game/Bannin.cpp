5bd1219b5604560db9cc9f61c5d51a6529949857
SatakeIsao <ca01234009@kawahara.ac.jp>
2023-10-08 19:51:00 +0900

    10.8

--- GameTemplate/Game/Bannin.cpp
+++ GameTemplate/Game/Bannin.cpp
@@ -10,6 +10,11 @@ bool Bannin::Start()
 {
 	m_modelRender.Init("Assets/modelData/Kemono.tks.tkm");
 	//m_modelRender.SetScale(0.3f, 0.3f, 0.3f);
+
+	//アニメーションイベント用の関数を設定する。
+	m_modelRender.AddAnimationEvent([&](const wchar_t* clipName, const wchar_t* eventName) {
+		OnAnimationEvent(clipName, eventName);
+		});
 	return true;
 }
 
@@ -20,11 +25,39 @@ void Bannin::Update()
 
 void Bannin::Move()
 {
-	//m_modelRender.SetPosition(m_position); //座標を設定。
+	m_modelRender.SetPosition(m_position); //座標を設定。
+}
+
+void Bannin::Bark()
+{
+	//吠えないなら処理しない。
+	if (m_banninState != enBanninState_Bark)
+	{
+		return;
+	}
+
+	//
 }
 
 void Bannin::PlayAnimation()
 {
+
+}
+
+void Bannin::OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName)
+{
+	//キーの名前が「bark_start」の時。
+	if (wcscmp(eventName, L"bark_start") == 0)
+	{
+		//吠えている
+		m_isUnderBark = true;
+	}
+	//キーの名前が「bark_end」の時。
+	else if (wcscmp(eventName, L"bark_end") == 0)
+	{
+		//吠えるのを終える。
+		m_isUnderBark = false;
+	}
 
 }
 
--- GameTemplate/Game/Bannin.h
+++ GameTemplate/Game/Bannin.h
@@ -4,7 +4,16 @@ class Bannin;
 class Bannin : public IGameObject
 {
 public:
-
+	//番人のステート。
+	enum EnBanninState {
+		enBanninState_Idle,			//待機。
+		enBanninState_Walk,			//歩き。
+		enBanninState_Run,			//走る。
+		enBanninState_Bark,			//吠える。
+		enBanninState_Down,			//HPが0。
+		enBanninState_Clear		    //クリアー。
+	};
+public:
 	Bannin();
 	bool Start();
 	void Update();
@@ -20,10 +29,40 @@ public:
 	{
 		return m_position;
 	}
+	//<summary>
+	//動ける状態(ステート)かどうかを取得。
+	// </summary>
+	//<returns>動ける状態(ステート)ならtrue。</returns>
+	bool IsEnableMove() const
+	{
+		return m_banninState != enBanninState_Bark &&
+			m_banninState != enBanninState_Down &&
+			m_banninState != enBanninState_Clear;
+	}
+private:
+
+	//アニメーションイベント用の関数。
+	void OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName);
+
+	//アニメーション
+	enum EnAnimationClip {
+		enAnimationClip_Idle,		//待機アニメーション。
+		enAnimationClip_Walk,		//歩きアニメーション。
+		enAnimationClip_Run,        //走りアニメーション。
+		enAnimationClip_Bark,		//吠えるアニメーション。
+		enAnimationClip_Down,		//ダウンアニメーション。
+		enAnimationClip_Winner,		//勝利アニメーション。
+		enAnimationClip_Num,		//アニメーションの数。
+	};
 
 	void Move();
+	void Bark();
 	void PlayAnimation();
+
+	AnimationClip m_animationClips[enAnimationClip_Num];
 	ModelRender m_modelRender;
 	Vector3 m_position; //座標。
+	EnBanninState m_banninState = enBanninState_Idle;
+	bool m_isUnderBark = false; //吠えているのならtrue。
 };
 
